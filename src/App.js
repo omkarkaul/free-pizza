5be0e7db66af0fd6a87304971a659c04389dbe92
omkar <omkar@xtra.co.nz>
2020-07-27 19:49:17 +1200

    init with most work done lol

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [randomNumber, setrandomNumber] = useState(0);
+
+  useEffect(() => {
+    generateRandomNumber();
+  }, []);
+  
+  function generateRandomNumber() {
+    let randomNumber = Math.floor(10000 + Math.random()*(99999 + 1 - 10000));
+
+    while(randomNumber % 3 !== 0) {
+      randomNumber = Math.floor(10000 + Math.random()*(99999 + 1 - 10000));
+    }
+
+    setrandomNumber(randomNumber);
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className = "container">
+        <h1 className = "randomNumber">{randomNumber}</h1>
+        <button className = "randomNumberButton" onClick = {generateRandomNumber}>More Pizza!</button>
+      </div>
+
+      <div className = "footer">
+        <p>Made by Omkar</p>
+        
+      </div>
+    </>
   );
 }
 
